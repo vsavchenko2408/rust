153aa46a0a00c77e42b06f6e0a88e62e7719a6c0
Der Meister <v.savchenko2408@gmail.com>
2025-12-28 01:31:03 +0100

    readln

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,10 +1,20 @@
+use std::io;
+
 fn main()
 {
- let mut a = 15;
- let b = 25;
- let c = add(a,b);
- a = 35;
- println!(" {}, {}",c, a);
+    let mut temp_a= String::new();
+    println!("Input a:");
+    io::stdin().read_line(&mut temp_a).expect("Error!");
+ 
+    let mut temp_b= String::new();
+    println!("Input b:");
+    io::stdin().read_line(&mut temp_b).expect("Error!");
+    
+    let a:i32 = temp_a.trim().parse().expect("Input a:");
+    let b:i32 = temp_b.trim().parse().expect("Input b:");
+    let result = add(a,b);
+
+ println!("Result: {}",result);
  
 }
 
