46b3cdb992b1321d0a1d74c71aefe34d9ac62d1f
Der Meister <v.savchenko2408@gmail.com>
2025-12-28 05:50:28 +0100

    array or vector

--- project/src/main.rs
+++ project/src/main.rs
@@ -2,22 +2,10 @@ use std::io;
 
 fn main()
 {
-    let mut temp_a= String::new();
-    println!("Input a:");
-    io::stdin().read_line(&mut temp_a).expect("Error!");
- 
-    let mut temp_b= String::new();
-    println!("Input b:");
-    io::stdin().read_line(&mut temp_b).expect("Error!");
-    
-    let a:i32 = temp_a.trim().parse().expect("Input a:");
-    let b:i32 = temp_b.trim().parse().expect("Input b:");
-    let result = add(a,b);
-
- println!("Result: {}",result);
- 
-}
-
-fn add(x: i32 , y: i32)-> i32{
-    x+y
+    let mut x = [5,3,6,2,8];
+   // x.push(9);
+   // x.push(4);
+    for n in x{
+    println!(" {:?}", n);
+    }
 }
