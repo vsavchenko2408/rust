839971c8faf519303991a7f9fe487880f5b586f8
Der Meister <v.savchenko2408@gmail.com>
2025-12-31 17:55:47 +0100

    try sort array

--- project/src/main.rs
+++ project/src/main.rs
@@ -2,10 +2,12 @@
 
 fn main()
 {
-let mut a = 15;
-let mut b = 25;
-swap(&mut a,&mut b);
-show(a,b);
+let mut arr = [5,4,3,2,1];
+sort(&mut arr);
+for i in arr
+{
+    println!("arr {}:  ", i);
+}
 }
 
 fn foo(x :&mut i32)
@@ -23,4 +25,18 @@ fn swap( a:&mut i32 , b:&mut i32 )
     let temp = *a;
     *a = *b;
     *b = temp;
+}
+
+fn sort(arr: &mut [i32;5])
+{
+    for x in arr
+    {
+        for y in arr
+        {
+            if x < y
+            {
+                swap(x, y);
+            }
+        }
+    }
 }
