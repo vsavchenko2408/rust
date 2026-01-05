489ec71b66990ea6514fe916e3d80153d9914360
Der Meister <v.savchenko2408@gmail.com>
2026-01-05 18:02:17 +0100

    project

--- project/src/main.rs
+++ project/src/main.rs
@@ -2,13 +2,24 @@
 
 fn main()
 {
-
+    let mut dt = Data {
+        data: 15,
+    };
+    dt.set_data(25);
+    println!("Data: {}", dt.get_data());
 }
 
 struct Data{
-    _Data: i32
+    data: i32
 }
 
 impl Data {
-    
+    fn set_data(&mut self, data:i32)
+    {
+        self.data = data;
+    }
+    fn get_data(&self) -> i32
+    {
+        self.data
+    }
 }
